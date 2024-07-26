e2a3d1094f57f33c47036cdee9351c00527d876c
João Victor de Paiva Albuquerque <123470825+JoaoVictorDePaiva@users.noreply.github.com>
2024-07-26 17:06:55 -0300

    V2

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import requests
+import functions
+import email.message
+import smtplib
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from bs4 import BeautifulSoup
-
-URLS = [
+Urls_Kabum = [
     "https://www.kabum.com.br/produto/102770/headset-gamer-havit-drivers-53mm-microfone-plugavel-p2-pc-ps4-xbox-one-preto-hv-h2002d",
     "https://www.kabum.com.br/produto/94555/mouse-gamer-redragon-cobra-chroma-rgb-10000dpi-7-botoes-preto-m711-v2",
     "https://www.kabum.com.br/produto/362905/cabo-dados-sata-3-ugreen-6gbps-hd-ssd-gravador-gamer-50cm",
     "https://www.kabum.com.br/produto/386758/hd-interno-western-digital-2tb-blue-5400-rpm-256mb-3-5-sata-iii-20ezaz"
 ]
 
-headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}
+Urls_nba = ["https://www.lojanba.com/p/tenis-nba-nike-giannis-immortality-3-masculino-preto+branco-JD8-3946-028"]
+
+header = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}
 
 produtos = []
 precos = []
@@ -21,35 +25,21 @@ adapter = HTTPAdapter(max_retries=retry)
 session.mount('http://', adapter)
 session.mount('https://', adapter)
 
-def info_kabum(url, headers, session):
-    try:
-        response = session.get(url, headers=headers)
-        response.raise_for_status()
-    except requests.RequestException as e:
-        print(f"Erro ao acessar a URL: {url} - {e}")
-        return
+for url in Urls_Kabum:
+    teste1 = functions.info_kabum(url, header, session)
+    if teste1:  # Verifica se a função retornou algo válido
+        price, title = teste1  # Desempacota a tupla
+        precos.append(price)
+        produtos.append(title)
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    title_element = soup.find('h1', {'class': 'sc-58b2114e-6 brTtKt'})
-    price_element = soup.find('b', {'class': 'regularPrice'})
+for url in Urls_nba:
+    teste2 = functions.info_nba(url, header, session)
+    if teste2: 
+        price, title = teste2 
+        precos.append(price)
+        produtos.append(title)
 
-    if title_element and price_element:
-        price = price_element.text.strip().replace("\xa0", "")
-        title = title_element.text.strip().split(',')[0]
-    else:
-        price_element = soup.find('h4', {'class': 'sc-5492faee-2 ipHrwP finalPrice'})
-        if title_element and price_element:
-            price = price_element.text.strip().replace("\xa0", "")
-            title = title_element.text.strip().split(',')[0]
-        else:
-            print(f"Erro ao encontrar título ou preço para a URL: {url}")
-            return
-
-    produtos.append(title)
-    precos.append(price)
-
-for url in URLS:
-    info_kabum(url, headers, session)
 
 print(produtos)
 print(precos)
+functions.send_email()
